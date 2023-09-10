cb9232e49b607c73bf14c70d3d427043f9c04e7f
w-al-z <109737988+w-al-z@users.noreply.github.com>
2023-09-10 20:59:17 +0100

    done
    first commit

---
+++ zuri.js
@@ -0,0 +1,30 @@
+// START CLOCK SCRIPT
+
+Number.prototype.pad = function (n) {
+  for (var r = this.toString(); r.length < n; r = 0 + r);
+  return r;
+};
+
+function updateClock() {
+  var now = new Date();
+  var milli = now.getMilliseconds(),
+    sec = now.getSeconds(),
+    min = now.getMinutes(),
+    hou = now.getHours();
+
+  var tags = ["mon", "d", "y", "h", "m", "s", "mi"],
+    corr = [hou.pad(2), min.pad(2), sec.pad(2), milli];
+  for (var i = 0; i < tags.length; i++)
+    document.getElementById(tags[i]).firstChild.nodeValue = corr[i];
+}
+
+function initClock() {
+  updateClock();
+  window.setInterval("updateClock()", 1);
+}
+
+// END CLOCK SCRIPT
+
+var dateObj = new Date();
+var weekday = dateObj.toLocaleString("default", { weekday: "long" });
+// weekday = "Saturday"
